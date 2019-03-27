d7be2f8b0b3ff8dd576ac6ee8c8e95337484209f
fei1355290116 <1355290116@qq.com>
2019-03-27 15:21:41 +0800

    提交

---
+++ js/new_file.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+	var imgs=document.getElementsByTagName("img");
+	imgs[0].onclick=function(){
+		document.body.style.background="url(images/357884c29c586ba9.jpg)"
+	}
+	imgs[1].onclick=function(){
+		document.body.style.background="url(images/2.jpg)"
+	}
+	imgs[2].onclick=function(){
+		document.body.style.background="url(images/1.jpg)"
+	}
+	imgs[3].onclick=function(){
+		document.body.style.background="url(images/4.jpg)"
+	}
+	imgs[4].onclick=function(){
+		document.body.style.background="url(images/5.jpg)"
+	}
+} 
